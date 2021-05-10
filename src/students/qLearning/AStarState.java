6a64f9df91fd8680b9ce57ae88711c0f0bac90d3
Jonathan Rotter <hydrogen31415@gmail.com>
2021-05-10 20:56:27 +0200

    cleanup & fixes

--- src/students/qLearning/AStarState.java
+++ src/students/qLearning/AStarState.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 import snakes.Coordinate;
 import snakes.Direction;
@@ -13,24 +11,11 @@ import snakes.Snake;
 
 public class AStarState implements State, Serializable {
 
-    private Coordinate appleRel;
-
-    private int[] wallDistance;
     private boolean[] nextToWall;
 
-    private Direction facing;
+    private Direction appleDir;
 
     public AStarState(Snake snake, Snake opponent, Coordinate mazeSize, Coordinate apple) {
-        Coordinate center = snake.getHead();
-        this.appleRel = new Coordinate(apple.x - center.x, apple.y - center.y);
-
-        Coordinate current = null;
-
-        wallDistance = new int[4];
-        wallDistance[0] = center.x;
-        wallDistance[1] = center.y;
-        wallDistance[2] = mazeSize.x - center.x;
-        wallDistance[3] = mazeSize.y - center.y;
         Coordinate head = snake.getHead();
         nextToWall = new boolean[4];
         nextToWall[0] = isLethal(snake, opponent, head.moveTo(Direction.UP), mazeSize); // center.x == 0;
@@ -40,26 +25,12 @@ public class AStarState implements State, Serializable {
 
         // get all directions but backward cause thats illegal.
 
-        Coordinate enemy = opponent.getHead();
-        Direction endir = enemy.getDirection(enemy);
-
         /*
          * Get the coordinate of the second element of the snake's body to prevent going
          * backwards
          */
-        Coordinate afterHeadNotFinal = null;
-        if (snake.body.size() >= 2) {
-            Iterator<Coordinate> it = snake.body.iterator();
-            it.next();
-            afterHeadNotFinal = it.next();
-        }
 
-        facing = null;
-        if (afterHeadNotFinal != null) {
-            facing = afterHeadNotFinal.getDirection(head);
-        }
-
-        facing = getMove(snake, apple);
+        appleDir = getMove(snake, apple);
 
     }
 
@@ -113,7 +84,7 @@ public class AStarState implements State, Serializable {
     public Direction[] getLegalActions() {
         Direction[] dirs = { Direction.DOWN, Direction.UP, Direction.LEFT, Direction.RIGHT };
 
-        Direction backwards = UtilStuff.getInverseDir(facing);
+        Direction backwards = UtilStuff.getInverseDir(appleDir);
 
         /*
          * The only illegal move is going backwards. Here we are checking for not doing
@@ -128,9 +99,11 @@ public class AStarState implements State, Serializable {
     public boolean equals(Object o) {
         if (o instanceof AStarState) {
             AStarState st = (AStarState) o;
-            return appleRel.equals(st.appleRel) && // */
-                    wallDistance[0] == st.wallDistance[0] && wallDistance[1] == st.wallDistance[1]
-                    && wallDistance[2] == st.wallDistance[2] && wallDistance[3] == st.wallDistance[3];
+            return appleDir.equals(st.appleDir) && //*/
+                nextToWall[0] == st.nextToWall[0] && 
+                nextToWall[1] == st.nextToWall[1] && 
+                nextToWall[2] == st.nextToWall[2] && 
+                nextToWall[3] == st.nextToWall[3];
         }
 
         return false;
@@ -138,22 +111,20 @@ public class AStarState implements State, Serializable {
 
     @Override
     public int hashCode() {
-        // https://www.baeldung.com/java-hashcode
-        int starter = wallDistance[0] + wallDistance[1] + wallDistance[2] + wallDistance[3];
-        int result = (int) (starter ^ (starter >>> 32));
-        result = 31 * result + wallDistance[0];
-        result = 31 * result + wallDistance[1];
-        result = 31 * result + wallDistance[2];
-        result = 31 * result + wallDistance[3];
-        result = 31 * result + appleRel.x;
-        result = 31 * result + appleRel.y;
+        //https://www.baeldung.com/java-hashcode
+        //int starter = wallDistance[0] + wallDistance[1] + wallDistance[2] + wallDistance[3];
+        int result = appleDir.hashCode();
+        result = 31 * result + (nextToWall[0] ? 1 : 0);
+        result = 31 * result + (nextToWall[1] ? 1 : 0);
+        result = 31 * result + (nextToWall[2] ? 1 : 0);
+        result = 31 * result + (nextToWall[3] ? 1 : 0);
         return result;
     }
 
     @Override
     public String toString() {
         String s = "[";
-        for (int e : wallDistance) {
+        for (boolean e : nextToWall) {
             s += e + ", ";
         }
         if (s.length() > 1) {
@@ -161,5 +132,4 @@ public class AStarState implements State, Serializable {
         }
         return s + "]";
     }
-
 }
