12bd75432b6b46889e7864afe92545d6691682b2
Jonathan Rotter <hydrogen31415@gmail.com>
2021-05-12 03:31:10 +0200

    fix

--- src/students/qLearning/AStarState.java
+++ src/students/qLearning/AStarState.java
@@ -10,8 +10,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import game.Point;
-import game.TPoint;
+// import game.Point;
+// import game.TPoint;
 import snakes.Coordinate;
 import snakes.Direction;
 import snakes.Snake;
